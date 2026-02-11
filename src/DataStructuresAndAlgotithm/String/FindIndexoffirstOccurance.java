e03120865d8f9bcf5ca12cac9ded2c9d204270bb
Sakshi Shelke <Sakshishelke316@gmail.com>
2026-02-11 22:50:05 +0530

    String

---
+++ src/DataStructuresAndAlgotithm/String/FindIndexoffirstOccurance.java
@@ -0,0 +1,21 @@
+package DataStructuresAndAlgotithm.String;
+
+public class FindIndexoffirstOccurance {
+    public int strStr(String haystack, String needle) {
+        if(haystack.length() < needle.length())
+        {
+            return -1;
+        }
+
+        for(int i=0;i<=haystack.length()-needle.length();i++)
+        {
+            if(haystack.substring(i,i+needle.length()).equals(needle))
+            {
+                return i;
+            }
+        }
+
+
+        return -1;
+    }
+}
